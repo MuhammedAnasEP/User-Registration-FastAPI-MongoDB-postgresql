a37bcfd44840ad9783b7d340463e951162c8d5a8
MuhammedAnasEP <anasep1110@gmail.com>
2023-11-10 21:46:05 +0530

    compleated

--- registration/main.py
+++ registration/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi import FastAPI, status, Depends, HTTPException, UploadFile, File
 from . import models
 from .database import engine, SessionLocal
-from .schemas import User
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -31,26 +31,36 @@ def check_email_exist(email, db):
     return True
 
 @app.post('/register',status_code=status.HTTP_201_CREATED)
-async def register(request: User, db: Session = Depends(get_db)):
-    if check_email_exist(request.email, db):
+async def register(first_name: str,
+    last_name: str,
+    email: str,
+    phone: str,
+    password: str,profile_picture: UploadFile = File(None), db: Session = Depends(get_db)):
+    if check_email_exist(email, db):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already exist")
     
-    hashed_password = pwd_context.hash(request.password)
+    hashed_password = pwd_context.hash(password)
 
-    user = models.User(first_name=request.first_name, last_name=request.last_name, password=hashed_password, email=request.email, phone=request.phone)
+    user = models.User(first_name=first_name, last_name=last_name, password=hashed_password, email=email, phone=phone)
     db.add(user)
     db.commit()
     db.refresh(user)
 
-    profile_picture = await request.profile_picture.read()
-    await mongo_db.profile_pictures.insert_one({"user_id": user.id, "profile_picture": profile_picture})
+    if profile_picture:
+        profile_picture_path = f"uploads/{user.id}_{profile_picture.filename}"
+        os.makedirs(os.path.dirname(profile_picture_path), exist_ok=True)
+        with open(profile_picture_path, "wb") as f:
+            f.write(profile_picture.file.read())
+
+
+        await mongo_db.profile_pictures.insert_one({"user_id": user.id, "profile_picture": profile_picture})
 
     return {"message": "User registered successfully"}
 
 
-@app.get("/user/{id}/", response_model=User)
+@app.get("/user/{id}/")
 async def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(User.id == user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
--- registration/schemas.py
+++ registration/schemas.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-from fastapi import UploadFile, File
 
 class User(BaseModel):
     first_name: str
@@ -7,4 +6,3 @@ class User(BaseModel):
     email: str
     phone: str
     password: str
-    profile_picture: UploadFile = File(...)
